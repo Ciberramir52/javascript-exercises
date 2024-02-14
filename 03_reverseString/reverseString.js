1112c7fd856ac5c31c5fdfef29d11e62358ecd34
Ramiro <63085235+Ciberramir52@users.noreply.github.com>
2024-02-13 19:07:20 -0500

    Solve 1-7 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // const strlen = str.length;
+    // let reversestr = "";
+    // for (let i = strlen - 1; i >= 0; i--) {
+    //     reversestr += str[i];
+    // }
+    // console.log(reversestr);
+    // return reversestr;
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    // for (const itemToRemove of itemsToRemove) {
+    //     // const index = array.indexOf(itemToRemove);
+    //     for (let index = array.indexOf(itemToRemove); index > -1; index = array.indexOf(itemToRemove)){
+    //         array.splice(index,1);
+    //     }
+    // }
+    const newArray = array.filter(item => !itemsToRemove.includes(item));
+    return newArray;
 };
 
 // Do not edit below this line
