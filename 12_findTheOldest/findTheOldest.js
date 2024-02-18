8b72531b952714d21770704673695d03b3294710
Ramiro <63085235+Ciberramir52@users.noreply.github.com>
2024-02-18 13:29:05 -0500

    Solve the rest of the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // people = people.map(person => {
+    //     if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+    //     return {
+    //         age: person.yearOfDeath - person.yearOfBirth,
+    //         ...person
+    //     };
+    // });
+    // return people.sort((a, b) => {
+    //     if (a.age < b.age) return 1;
+    //     if (a.age > b.age) return -1;
+    //     return 0;
+    // })[0];
+    return people.reduce((acc, current) => {
+        const accAge = getAge(acc.yearOfBirth, acc.yearOfDeath);
+        const currAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return currAge > accAge ? current : acc;
+    })
 };
+
+function getAge (birth, death) {
+    if (!death) death = new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
